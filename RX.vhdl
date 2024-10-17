138c49b4b2ef7051f207b57d8b414e0e45bf408c
Mosazghi <fest121@hotmail.com>
2024-10-17 11:49:58 +0200

    comment

--- RX.vhdl
+++ RX.vhdl
@@ -1,1 +1,13 @@
-entity
+entity RX is 
+  Port ( clk : in  STD_LOGIC;
+         reset : in  STD_LOGIC;
+         data_in : in  STD_LOGIC;
+         data_out : out  STD_LOGIC;
+         data_valid : out  STD_LOGIC;
+         data_ready : in  STD_LOGIC);
+end RX;
+
+
+
+-- case addr 
+-- when 100 => 
