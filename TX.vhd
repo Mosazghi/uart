a14062888f685727305f7de5bb54fcc43d60855d
Emiljohanseen <148060337+Emiljohanseen@users.noreply.github.com>
2024-10-17 13:13:00 +0200

    Update TX.vhd

--- TX.vhd
+++ TX.vhd
@@ -5,8 +5,11 @@ use IEEE.STD_LOGIC_ARITH.all;
 entity TX is
 	port(
 	clk : in std_logic;
-	reset : in std_logic);
-	
+	reset : in std_logic;
+	par_sel : in std_logic;
+	baud_sel : in std_logic;
+	tx_busy : out std_logic;
+	tx_data : out std_logic);
 end TX;
 
 --case addr
