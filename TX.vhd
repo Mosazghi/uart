00858fea9385ca0a9c5f9e17aabf627c9ff7dd48
Emiljohanseen <148060337+Emiljohanseen@users.noreply.github.com>
2024-10-18 08:32:08 +0200

    Update TX.vhd

--- TX.vhd
+++ TX.vhd
@@ -8,9 +8,30 @@ entity TX is
 	reset : in std_logic;
 	par_sel : in std_logic;
 	baud_sel : in std_logic;
+	data_bus : inout std_logic_vector(11 downto 0);
 	tx_busy : out std_logic;
-	tx_data : out std_logic);
+	TxD : out std_logic);
 end TX;
 
+architecture RTL of TX is 
+  -- ADDR:
+  -- 000 = TxConfig (NA - Parity - Baud)
+  -- 001 = TxData (Data in)
+  -- 010 = TxStatus (NA - Busy)
+signal addr : std_logic_vector := data_bus(2 downto 0);
+signal data : std_logic_vector := data_bus(10 downto 3);
+signal tx_data : std_logic_vector(7 downto 0);
+
+	begin 
+ p_main: process(clk, reset) is 
+          begin
+            if reset = '1' then
+              -- reset something
+            elsif rising_edge(clk) then
+                
+            end if;
+          end process p_main;
+
+end architecture RTL;
 --case addr
 --when 000 => baud <= data_bus(2 downto 0); parity <= data_bus(4 downto 3);
