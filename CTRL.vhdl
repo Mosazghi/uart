ed2e3883d6f14d167c06fd7a2df1d3df7102e551
thomaks <thomaks@stud.ntnu.no>
2024-10-24 14:19:24 +0200

    Big Moves on the CTRL module (updated and cleaned)

--- CTRL.vhdl
+++ CTRL.vhdl
@@ -2,112 +2,84 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity CTRL is
-port (
-    clk        : in std_logic;                           -- Klokke signal
-    rst        : in std_logic;                           -- Reset signal
-    snd        : in std_logic;
-	 rx_ready	: in std_logic;
-	 tx_busy		: in std_logic;
-    baud_sel   : in std_logic_vector(2 downto 0);
-    par_sel    : in std_logic_vector(1 downto 0);
-	 rx_data		: in std_logic_vector(7 downto 0);
-    -- data_bus 	: inout std_logic_vector(10 downto 0); 	-- rd + wr + addr + data 
-    snd_led    : out std_logic;
-	 tx_data    : out std_logic_vector(7 downto 0));
-	 
-	 /*
-    tx_ready   : in std_logic;
-    rx_data    : in std_logic_vector(7 downto 0);
-    rx_ready   : in std_logic;
-    */
-
-end entity CTRL;	
-
-u_ctrl : CTRL
-    port map (
-        clk         => clk,
-        rst         => rst,
-        snd_led 	  => snd_led,
-        tx_data     => tx_data,               -- Data to be transmitted (loopback)
-        rx_data     => rx_data,               -- Data received
-        tx_ready    => tx_ready,              -- CTRL indicates when ready for TX
-        rx_ready    => rx_ready,              -- Data ready signal from RX module
-        led_ctrl    => led_ctrl,              -- Control LED for data reception
-        baud_sel => baud_sel,           -- Baud rate control signal
-        par_sel => par_sel        -- Parity control signal
-    );
-
+entity CTRL is port(
+	clk 		: in 		std_logic;
+	rst		: in 		std_logic; 
+	snd		: in 		std_logic;
+	baud_sel	: in 		std_logic_vector(2 downto 0);
+	par_sel	: in 		std_logic_vector(1 downto 0);
+	databus	: inout 	std_logic_vector(7 downto 0);
+	snd_led	: out 	std_logic);
+end entity CTRL;
 
 architecture RTL of CTRL is
-	---------------------------------
-	-- Type
-	---------------------------------
-	type state_type is (Idle, Get, Send);
+	type	 State_Type is (Idle, Get, Send);
+	signal State : State_Type;
+
+	signal RxData 	: std_logic_vector(7 downto 0);
+	signal TxData 	: std_logic_vector(7 downto 0);
+	signal adr		: std_logic_vector(2 downto 0);
 	
-	---------------------------------
-	-- Signal
-	---------------------------------
-	signal adresse : data_bus(2 downto 0);
-	signal data		: data_bus(7 downto 0	);
-	signal state 	: state_type;
-	
-	signal config : (4 downto 0);
 	
 	
 begin
-	process(clk, rst) is begin
+	
+	process (clk, rst) is
 		if (rst = '0') then 
-			state <= Idle; 
+			State <= Idle;
 			snd_led <= '1';
-			adr<= "100";
-				data_bus<= config;
-				config(2 downto 0) <= baud_sel;
-				config(4 downto 3) <= par_sel;
-				
-			adr<= "000";
-				data_bus<= config;
-				config(2 downto 0) <= baud_sel;
-				config(4 downto 3) <= par_sel;
-					
-					
-			rx_data <= (others <= '0');
-			tx_data <= (others <= '0');
 			
-		
+			adr <= "100";	-- Setter adresse til Rx config
+			RxData(2 downto 0) <= baud_sel;
+			RxData(4 downto 3) <= par_sel;
+			databus <= RxData;	-- Sender baud og parity over databussen
+			
+			adr <= "000";	-- Setter adresse til Tx config 
+			TxData(2 downto 0) <= baud_sel;
+			TxData(4 downto 3) <= par_sel;
+			databus <= TxData;	-- Sender baud og parity over databussen
+			
+			-- Tilbakestiller databussen og 
+			databus <= (others <= '0');
+			RxData, TxData <= databus;
+			
 		elsif (rising_edge(clk)) then
-			
-			case state is 
-			
-				when Idle =>
-					if (rx_ready = '1') then
-					
-					
-					
-					
+			case State is
+				when Idle =>	
+					-- Sjekker Rx status
+					if 	databus(3 downto 3) = '1' then
+						-- Parity Error
+					elsif databus(2 downto 2) = '1' then
+						-- Data Lost
+					elsif databus(1 downto 1) = '1' then
+						-- FIFO Full
+						State <= Get;
+					elsif databus(0 downto 0) = '1' then
+						-- FIFO Empty
+					else
+						state <= Idle;
 					end if;
 					
 				when Get =>
-					if (tx_busy = '0' and snd = '1') then
-						tx_data <= std_logic_vector(unsigned(rx_data));
+					adr <= "101";		-- Setter adresse til å motta data fra Rx
+					if (RxData /= databus) then	-- Venter på dataen er mottat fra Rx
+						TxData <= databus;	-- Gjør dataen klar for sending til Tx
+						state <= Send;		-- Setter status til sending
+					else
+						State <= Get;
+					end if;
+
+				when Send =>
+					adr <= "010";							-- Sjekker om Tx er klar til å motta data
+					if (databus = "00000000" and snd = '1') then	-- Venter til Tx er klar og sendeknapp er initiert
+						adr <= "001";							-- Setter adresse for sending av data til Tx
+						databus <= TxData; 					-- Sender data til Tx
+						databus <= (others <= '0');		-- Tilbakestiller databussen og gjøres klar til Idle status etter sending
+						state <= Idle;
+					else 
 						state <= Send;
 					end if;
-					
-				when Send =>
-					
-				when others => null;
 			end case;
 		end if;
 	end process;
-	
-	
-end architecture
-
-
-
-
-
-
- 
-
-
+end architecture;
