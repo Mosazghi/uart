c00fcc42e5e27ce465894d0cab576e35de56123a
Luai9897 <luaiattaf97@gmail.com>
2024-10-24 10:58:33 +0200

    Update UART.vhd

--- UART.vhd
+++ UART.vhd
@@ -6,18 +6,16 @@ entity CTRL is
 port (
     clk        : in std_logic;                            -- Klokke signal
     rst        : in std_logic;                            -- Reset signal
-    snd        : in std_logic;
-    baud_sel    : in std_logic_vector(2 downto 0);
-    par_sel    : in std_logic_vector(1 downto 0);
-    buss        : inout std_logic_vector(12 downto 0);
-    snd_led    : out    std_logic;
-	 tx_data    : out    std_logic_vector(7 downto 0);
-	 
-    tx_ready    : in std_logic;
-    rx_data    : in std_logic_vector(7 downto 0);
-    rx_ready    : in std_logic;
-
-    rd :out std_logic_vector
-    wr :out std_logic_vector
+    snd        : in std_logic;                            --knapp
+    baud_sel   : in std_logic_vector(2 downto 0);         -- Baud rate
+    par_sel    : in std_logic_vector(1 downto 0);         -- Paritet
+    buss       : inout std_logic_vector(12 downto 0);     -- Adder + Data på bussen
+    snd_led    : out std_logic;				  -- LED for å indikere aktivitet
+    tx_data    : out std_logic_vector(7 downto 0);
+    tx_ready   : in std_logic;                            -- TX klar signal
+    rx_data    : in std_logic_vector(7 downto 0);         -- Mottat data
+    rx_ready   : in std_logic;                            -- RX klar signal
+    rd         :out std_logic_vector                      -- Read signal
+    wr         :out std_logic_vector			  -- Write signal
 
 end entity CTRL;
