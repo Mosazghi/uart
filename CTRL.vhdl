a8f58d9f56de258e1278d3424a4dbe161ae7dfee
ninjacop1 <wilhelm.andersen98@gmail.com>
2024-10-25 08:49:15 +0200

    gjorde om config til en state machine
    gjorde om til state machine(for å unngå at den hopper over noe) å lå til write signaler på begge.

--- CTRL.vhdl
+++ CTRL.vhdl
@@ -10,6 +10,8 @@ entity CTRL is port(
 	par_sel	: in 		std_logic_vector(1 downto 0);
 	databus	: inout 	std_logic_vector(7 downto 0);
 	snd_led	: out 	std_logic);
+	wr : out std_logic);
+	rd : out std_logic);
 end entity CTRL;
 
 architecture RTL of CTRL is
@@ -20,28 +22,66 @@ architecture RTL of CTRL is
 	signal TxData 	: std_logic_vector(7 downto 0);
 	signal adr		: std_logic_vector(2 downto 0);
 	
-	
-	
 begin
-	
-	process (clk, rst) is
-		if (rst = '0') then 
-			State <= Idle;
-			snd_led <= '1';
-			
-			adr <= "100";	-- Setter adresse til Rx config
-			RxData(2 downto 0) <= baud_sel;
-			RxData(4 downto 3) <= par_sel;
-			databus <= RxData;	-- Sender baud og parity over databussen
-			
-			adr <= "000";	-- Setter adresse til Tx config 
-			TxData(2 downto 0) <= baud_sel;
-			TxData(4 downto 3) <= par_sel;
-			databus <= TxData;	-- Sender baud og parity over databussen
-			
-			-- Tilbakestiller databussen og 
-			databus <= (others <= '0');
-			RxData, TxData <= databus;
+    u_ctrl : CTRL
+    port map (
+        clk         => clk,
+        rst         => rst,
+        snd => snd,
+        --tx_ready    => tx_ready,              -- CTRL indicates when ready for TX
+        --rx_ready    => rx_ready,              -- Data ready signal from RX module
+        snd_led    => snd_led,              -- Control LED for data reception
+        baud_sel => baud_sel,           -- Baud rate control signal
+        par_sel => par_sel        -- Parity control signal
+    );
+process (clk, rst) --- konfiguerer rx og tx ved start
+begin
+    if (rst = '0') then
+        State <= start;
+        snd_led <= '1';
+        
+        -- Initialize variables and states
+        adr <= (others => '0');
+        databus <= (others => '0');
+        RxData <= (others => '0');
+        TxData <= (others => '0');
+        wr <= '0';  -- Disable write at reset
+        rd <= '0';  -- Disable read at reset
+        
+    elsif rising_edge(clk) then 
+        case State is
+        
+            when start =>
+                --konfigurerer rx
+                adr <= "100";  -- Addresse rx
+                RxData(2 downto 0) <= baud_sel;
+                RxData(4 downto 3) <= par_sel;
+                databus <= RxData;
+                wr <= '1';  -- skrive
+                State <= Write_Tx_Config;
+            
+            when Write_Tx_Config =>
+                --konfigurerer tx
+                adr <= "000";  -- Addresse  Tx 
+                TxData(2 downto 0) <= baud_sel;
+                TxData(4 downto 3) <= par_sel;
+                databus <= TxData;
+                wr <= '1';  -- skrive
+                State <= Finish;
+            
+            when Finish =>
+                -- etter inialisering
+                wr <= '0';  -- slutt å skrive
+                databus <= (others => '0');
+					 RxData, TxData <= databus;
+                adr <= (others => '0');
+                State <= start;
+                -- addresse bus er tatt til null
+            when others =>
+                State <= start;
+				end case;
+			end if;
+		end process;
 			
 		elsif (rising_edge(clk)) then
 			case State is
@@ -68,7 +108,9 @@ begin
 					else
 						State <= Get;
 					end if;
-
+					
+					
+					
 				when Send =>
 					adr <= "010";							-- Sjekker om Tx er klar til å motta data
 					if (databus = "00000000" and snd = '1') then	-- Venter til Tx er klar og sendeknapp er initiert
