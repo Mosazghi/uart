1557dd0f006f13bba9b60eae1e87e3b9306c32a9
Luai9897 <luaiattaf97@gmail.com>
2024-10-31 10:25:55 +0100

    ferdig med UART entity

--- UART.vhd
+++ UART.vhd
@@ -1,27 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.STD_LOGIC_ARITH.all;
-use ieee.numeric_std.all;
-
-entity CTRL is
-port (
-    clk        : in std_logic;                            -- Klokke signal
-    rst        : in std_logic;                            -- Reset signal
-    snd        : in std_logic;                            --knapp
-    baud_sel   : in std_logic_vector(2 downto 0);         -- Baud rate
-    par_sel    : in std_logic_vector(1 downto 0);         -- Paritet
-    buss       : inout std_logic_vector(12 downto 0);     -- Adder + Data på bussen
-    snd_led    : out std_logic;				  					-- LED for å indikere aktivitet
-    tx_data    : out std_logic_vector(7 downto 0);
-    tx_ready   : in std_logic;                            -- TX klar signal
-    rx_data    : in std_logic_vector(7 downto 0);         -- Mottat data
-    rx_ready   : in std_logic;                            -- RX klar signal
-    rd         :out std_logic_vector                      -- Read signal
-    wr         :out std_logic_vector			 				 -- Write signal
-	 );
-	 
-
-end entity CTRL;
 
 
 entity UART is
@@ -34,21 +12,19 @@ entity UART is
         snd_led  : out std_logic;                         -- LED indicating received/transmitted
         baud_sel : in std_logic_vector(2 downto 0);       -- Baud rate selection SW0, SW1 and  SW2
         par_sel  : in std_logic_vector(1 downto 0);       -- Parity selection SW3 and SW4
-        data_bus  : inout std_logic_vector(7 downto 0)    -- Data bus (shared for Tx and Rx)
+        
     );
 end entity UART;
 
 architecture Behavioral of UART is
-    -- Signal to connect internal components
-    signal tx_data_out  : std_logic_vector(7 downto 0);     -- Data to transmit
-    signal rx_data_in   : std_logic_vector(7 downto 0);     -- Data received
-    signal tx_ready   	: std_logic;                        -- TX ready signal
-    signal rx_ready   	: std_logic;                        -- RX ready signal
+    -- Signal to connect internal components	
+	 signal data_bus		: std_logic_vector(7 downto 0);		-- Shared 8-bit data bus (internal only)
     signal wr         	: std_logic;                        -- Write signal
     signal rd         	: std_logic;    							-- Read signal
-	 signal adder      	: std_logic_vector(2 downto 0) 		-- Address for register selection
-	 
+	 signal adder      	: std_logic_vector(2 downto 0);		-- Address for register selection
+	
 begin
+		
     -- CTRL module
     u_ctrl: entity work.CTRL
         port map (
@@ -59,10 +35,6 @@ begin
             par_sel  => par_sel,
             data_bus => data_bus,
             snd_led  => snd_led,
-				tx_data  => tx_data_out,
-            tx_ready => tx_ready,
-            rx_data  => rx_data_in,
-            rx_ready => rx_ready,
 				adder		=> adder,
             wr       => wr,
             rd       => rd
@@ -89,7 +61,7 @@ begin
             RxD      => RxD,                             -- Receive data input
             data_bus => data_bus,                          -- Shared data bus
             addr     => adder,         					-- Address bus
-            rd       => rd,                             -- Read signal from CTRL
+            Rd       => rd,                             -- Read signal from CTRL
             wr       => wr                             -- Write signal from CTRL
         );
 end Behavioral;
